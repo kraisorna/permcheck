3f862c76fef0c02b69f086a81b28b4d74c3ce805
Kraisorn Arjharn <kraisorna@aol.com>
2017-03-13 18:30:13 +0700

    First local commit

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PermCheck
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution s = new Solution();
+            //int[] A = { 4,1,3,2 };
+            int[] A = { 4,1,3 };
+            Console.WriteLine(s.solution(A));
+            Console.ReadLine();
+        }
+    }
+
+    class Solution
+    {
+        public int solution(int[] A)
+        {
+            Array.Sort(A);
+            int N = A.Length;
+            int check = 1;
+            for (int i = 0; i < N; i++)
+            {
+                if (A[i] != i+1) check=0;
+            }
+            return check;
+        }
+    }
+}
